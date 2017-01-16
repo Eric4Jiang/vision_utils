964ebbfd4a59107e4a883da44805a6f3a4e0f1d9
August <augustt198@gmail.com>
2017-01-16 16:09:19 -0500

    Add IOU

--- contours.py
+++ contours.py
@@ -1,6 +1,39 @@
 import cv2
 import numpy as np
 
+# compute intersection over union (IOU) of two contours.
+# IOU = (area of intersection) / (area of union)
+#
+# returns: (area of intersection, area of union)
+def iou_frac(c1, c2):
+    max_x = max(c1[:,:,0].max(), c2[:,:,0].max())
+    max_y = max(c1[:,:,1].max(), c2[:,:,1].max())
+    # just in case there is some float business going on
+    max_x = int(max_x + 1)
+    max_y = int(max_x + 1)
+
+    canvas1 = np.zeros((max_x, max_y), dtype=np.uint8)
+    canvas2 = canvas1.copy()
+    cv2.fillPoly(canvas1, [c1], 1) 
+    cv2.fillPoly(canvas2, [c2], 1)
+
+    canvas = canvas1 + canvas2
+
+    intersection = np.sum(canvas == 2)
+    union = np.sum(canvas != 0)
+    return intersection, union
+
+# compute intersection over union (IOU) of two contours.
+# IOU = (area of intersection) / (area of union)
+def iou(c1, c2):
+    i, u = iou_frac(c1, c2)
+    return float(i) / u
+
+# compute the center of contour. returns (cx, cy)
+def center(contour):
+    M = cv2.moments(contour)
+    return M["m10"]/M["m00"], M["m01"]/M["m00"]
+
 # sort a list of points in clockwise order around a center
 #
 # arguments:
@@ -38,10 +71,7 @@ def sort_pts(pts_list, center):
 # arguments:
 #   * contour: a numpy array of points with shape (n_points, 1, 2)
 def sort_contour(contour):
-    M = cv2.moments(contour)
-    center = (M["m10"]/M["m00"], M["m01"]/M["m00"])
-    
-    return sort_contour_center(contour, center)
+    return sort_contour_center(contour, center(contour))
 
 # sorts a contour so that its points are in clockwise order
 #
